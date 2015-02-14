af53522a07fdaf8660c6c333b28ccb570155e875
Nicholas Arner <nicholasarner@gmail.com>
2015-02-13 21:27:52 -0500

    Added Mac PlayAudioFile

---
+++ OSX/Objective-C/PlayAudioFile/PlayAudioFile/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  PlayAudioFile
+//
+//  Created by Nicholas Arner on 2/13/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
