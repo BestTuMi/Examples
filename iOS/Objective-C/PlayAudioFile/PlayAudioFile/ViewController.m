118cc941bed4dc34824a61e263b1c6f7df33a647
Nicholas Arner <nicholasarner@gmail.com>
2015-02-13 20:11:51 -0500

    Added PlayAudioFile

---
+++ iOS/Objective-C/PlayAudioFile/PlayAudioFile/ViewController.m
@@ -0,0 +1,18 @@
+//
+//  ViewController.m
+//  PlayAudioFile
+//
+//  Created by Aurelius Prochazka on 6/30/14.
+//  Copyright (c) 2014 Aurelius Prochazka. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
