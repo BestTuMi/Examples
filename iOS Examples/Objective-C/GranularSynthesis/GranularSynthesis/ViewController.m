f12c2c728a89d23a643f64378665f811f569ac5d
Aurelius Prochazka <aure@aure.com>
2015-04-01 14:16:41 -0700

    Significantly simplified granular example with AKPropertySlider

--- iOS Examples/Objective-C/GranularSynthesis/GranularSynthesis/ViewController.m
+++ iOS Examples/Objective-C/GranularSynthesis/GranularSynthesis/ViewController.m
@@ -9,8 +9,8 @@
 #import "ViewController.h"
 
 #import "AKFoundation.h"
-#import "AKTools.h"
 #import "GranularInstrument.h"
+#import "AKPropertySlider.h"
 
 
 @interface ViewController ()
@@ -19,12 +19,12 @@
     BOOL isGranularInstrumentPlaying;
 }
 
-@property (strong, nonatomic) IBOutlet UISlider *mixSlider;
-@property (strong, nonatomic) IBOutlet UISlider *frequencySlider;
-@property (strong, nonatomic) IBOutlet UISlider *durationSlider;
-@property (strong, nonatomic) IBOutlet UISlider *densitySlider;
-@property (strong, nonatomic) IBOutlet UISlider *frequencyVariationSlider;
-@property (strong, nonatomic) IBOutlet UISlider *frequencyVariationDistributionSlider;
+@property (strong, nonatomic) IBOutlet AKPropertySlider *mixSlider;
+@property (strong, nonatomic) IBOutlet AKPropertySlider *frequencySlider;
+@property (strong, nonatomic) IBOutlet AKPropertySlider *durationSlider;
+@property (strong, nonatomic) IBOutlet AKPropertySlider *densitySlider;
+@property (strong, nonatomic) IBOutlet AKPropertySlider *frequencyVariationSlider;
+@property (strong, nonatomic) IBOutlet AKPropertySlider *frequencyVariationDistributionSlider;
 
 @end
 
@@ -33,33 +33,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     
     granularInstrument = [[GranularInstrument alloc] init];
     [AKOrchestra addInstrument:granularInstrument];
-    [self updateSliders];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)updateSliders
-{
-    [AKTools setSlider:self.mixSlider
-          withProperty:granularInstrument.mix];
-    [AKTools setSlider:self.frequencySlider
-          withProperty:granularInstrument.frequency];
-    [AKTools setSlider:self.durationSlider
-          withProperty:granularInstrument.duration];
-    [AKTools setSlider:self.densitySlider
-          withProperty:granularInstrument.density];
-    [AKTools setSlider:self.frequencyVariationSlider
-          withProperty:granularInstrument.frequencyVariation];
-    [AKTools setSlider:self.frequencyVariationDistributionSlider
-          withProperty:granularInstrument.frequencyDistribution];
+    
+    _mixSlider.property       = granularInstrument.mix;
+    _frequencySlider.property = granularInstrument.frequency;
+    _durationSlider.property  = granularInstrument.duration;
+    _densitySlider.property   = granularInstrument.density;
+    _frequencyVariationSlider.property = granularInstrument.frequencyVariation;
+    _frequencyVariationDistributionSlider.property = granularInstrument.frequencyDistribution;
 }
 
 - (IBAction)toggleGranularInstrument:(id)sender
@@ -73,28 +56,5 @@
     }
 }
 
-- (IBAction)mixChanged:(UISlider *)sender {
-    [AKTools setProperty:granularInstrument.mix withSlider:sender];
-}
-
-- (IBAction)frequencyChanged:(UISlider *)sender {
-    [AKTools setProperty:granularInstrument.frequency withSlider:sender];
-}
-
-- (IBAction)durationChanged:(UISlider *)sender {
-    [AKTools setProperty:granularInstrument.duration withSlider:sender];
-}
-
-- (IBAction)denistyChanged:(UISlider *)sender {
-    [AKTools setProperty:granularInstrument.density withSlider:sender];
-}
-
-- (IBAction)frequencyVariationChanged:(UISlider *)sender {
-    [AKTools setProperty:granularInstrument.frequencyVariation withSlider:sender];
-}
-
-- (IBAction)frequencyVariationDistributionChanged:(UISlider *)sender {
-    [AKTools setProperty:granularInstrument.frequencyDistribution withSlider:sender];
-}
 
 @end
