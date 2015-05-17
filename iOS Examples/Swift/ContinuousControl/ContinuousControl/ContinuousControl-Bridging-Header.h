3f59408d9c287c7c80480604b450bfe1b05ee3a4
Matthew Michael <matthew.fecher@gmail.com>
2015-05-17 14:47:25 -0600

    Added ContinuousControl Swift Example

---
+++ iOS Examples/Swift/ContinuousControl/ContinuousControl/ContinuousControl-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  ContinuousControl-Bridging-Header.h
+//  TableDemo
+//
+//  Created by Matthew Fecher on 5/17/15.
+//  Copyright (c) 2015 audiokit.io. All rights reserved.
+//
+
+#import "AKFoundation.h"
+#import "AKAudioOutputPlot.h"
+#import "AKTools.h"
