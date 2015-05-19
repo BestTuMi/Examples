b695b6985239d170090d22b033113b1646bd57de
Aurelius Prochazka <aure@aure.com>
2015-05-19 10:52:48 -0700

    Code tidying

--- iOS Examples/Swift/ContinuousControl/ContinuousControl/TweakableInstrument.swift
+++ iOS Examples/Swift/ContinuousControl/ContinuousControl/TweakableInstrument.swift
@@ -7,30 +7,30 @@
 //
 
 class TweakableInstrument: AKInstrument {
-    
+
     // INSTRUMENT CONTROLS =====================================================
-    
-    var amplitude     = AKInstrumentProperty(value: 0.1, minimum: 0,  maximum: 0.8)
-    var frequency     = AKInstrumentProperty(value: 220, minimum: 110, maximum: 880)
-    var modulation    = AKInstrumentProperty(value: 0.5,   minimum: 0.25,  maximum: 2.2)
-    var modIndex      = AKInstrumentProperty(value: 1,  minimum: 0,  maximum: 25)
-    
+
+    var amplitude     = AKInstrumentProperty(value: 0.1, minimum: 0,    maximum: 0.8)
+    var frequency     = AKInstrumentProperty(value: 220, minimum: 110,  maximum: 880)
+    var modulation    = AKInstrumentProperty(value: 0.5, minimum: 0.25, maximum: 2.2)
+    var modIndex      = AKInstrumentProperty(value: 1,   minimum: 0,    maximum: 25)
+
     // INSTRUMENT DEFINITION ===================================================
-    
+
     override init() {
         super.init()
-        
+
         addProperty(amplitude)
         addProperty(frequency)
         addProperty(modulation)
         addProperty(modIndex)
-        
+
         let fmOscillator = AKFMOscillator()
         fmOscillator.amplitude = amplitude
         fmOscillator.baseFrequency = frequency
         fmOscillator.modulatingMultiplier = modulation
         fmOscillator.modulationIndex = modIndex
-        
+
         // AUDIO OUTPUT
         setAudioOutput(fmOscillator)
     }
